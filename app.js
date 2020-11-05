c0afa9515654330e5187ab958e0e83db989c0378
Sebastian Mocanu <diemetal851@gmail.com>
2020-11-05 21:57:24 +0200

    income tracking

--- app.js
+++ app.js
@@ -31,10 +31,46 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+// Update the balance, income and Expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item) , 0).toFixed(2);
+  const income = amounts.filter(item => item > 0)
+                        .reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const expense = (amounts.filter(item => item < 0)
+                         .reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  moneyPlus.innerText = `$${income}`;
+  moneyMinus.innerText = `$${expense}`;
+}
+
 // Init app
 function init() {
   list.innerHTML = '';
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
